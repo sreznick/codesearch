22dbdda2355307f9016ad6a5c3db9a878e05f672
syberlord <alex.suvaloff@gmail.com>
2025-01-12 18:33:50 +0300

    Add tests

--- app/src/main/java/org/example/StringConstantQuery.java
+++ app/src/main/java/org/example/StringConstantQuery.java
@@ -20,63 +20,42 @@ import static org.example.JavaSourceIndexer.indexJavaSources;
 public class StringConstantQuery {
     private static final Logger logger = LogManager.getLogger();
 
-    private static void findBoolLiteralsTest(Boolean flag) {
-        return;
-    }
+    public static StringBuilder logBuilder = new StringBuilder();
 
-    private static void runQuery(String queryString, String type, Consumer<Document> documentConsumer) {
-        String str = "Hello, Lucene";
-        findBoolLiteralsTest(true);
 
+    private static void runQueryWithConfig(String queryString, String type, boolean isFuzzy, Consumer<Document> documentConsumer) {
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
         try (MMapDirectory directory = new MMapDirectory(Paths.get("index"));
              IndexReader reader = DirectoryReader.open(directory)) {
 
             IndexSearcher searcher = new IndexSearcher(reader);
+
             Query query = new BooleanQuery.Builder()
-                    .add(new TermQuery(new Term("content", queryString)), BooleanClause.Occur.MUST)
+                    .add(isFuzzy ? new FuzzyQuery(new Term("content", queryString), 2)
+                            : new TermQuery(new Term("content", queryString)), BooleanClause.Occur.MUST)
                     .add(new TermQuery(new Term("type", type)), BooleanClause.Occur.MUST)
                     .build();
 
-            logger.info("Запрос на {}: {}", type, queryString);
+            String queryTypeMessage = isFuzzy ? " (с неточностями)" : "";
+            String logMessage = "Запрос на " + type + queryTypeMessage + ": " + queryString;
+            logger.info(logMessage);
+            logBuilder.append(logMessage).append("\n");
 
             TopDocs results = searcher.search(query, 100000);
-            logger.info("Найдено совпадений: {}", results.totalHits.toString().substring(0, Math.max(0, results.totalHits.toString().length() - 4)));
+            logMessage = "Найдено совпадений " + (isFuzzy ? "с" : "c") + " " + queryString + ": " +
+                    results.totalHits.toString().substring(0, Math.max(0, results.totalHits.toString().length() - 5));
+            logger.info(logMessage);
+            logBuilder.append(logMessage).append("\n");
 
             processResults(executor, results, searcher, documentConsumer);
 
         } catch (IOException e) {
-            logger.error("Ошибка при выполнении запроса: {}", e.getMessage(), e);
+            String errorMessage = "Ошибка при выполнении запроса: " + e.getMessage();
+            logger.error(errorMessage, e);
+            logBuilder.append(errorMessage).append("\n");
         }
     }
-
-    private static void runFuzzyQuery(String queryString, String type, Consumer<Document> documentConsumer) {
-        try (MMapDirectory directory = new MMapDirectory(Paths.get("index"));
-             IndexReader reader = DirectoryReader.open(directory)) {
-
-            IndexSearcher searcher = new IndexSearcher(reader);
-
-            Query fuzzyQuery = new BooleanQuery.Builder()
-                    .add(new FuzzyQuery(new Term("content", queryString), 2), BooleanClause.Occur.MUST)
-                    .add(new TermQuery(new Term("type", type)), BooleanClause.Occur.MUST)
-                    .build();
-
-            logger.info("Запрос (с неточностями) на {}: {}", type, queryString);
-
-            TopDocs results = searcher.search(fuzzyQuery, 100000);
-            logger.info("Найдено совпадений: {}", results.totalHits.toString());
-
-            for (ScoreDoc scoreDoc : results.scoreDocs) {
-                Document doc = searcher.storedFields().document(scoreDoc.doc);
-                documentConsumer.accept(doc);
-            }
-
-        } catch (IOException e) {
-            logger.error("Ошибка при выполнении нечеткого запроса: {}", e.getMessage(), e);
-        }
-    }
-
 
     private static void processResults(ExecutorService executor, TopDocs results, IndexSearcher searcher, Consumer<Document> documentConsumer) {
         Set<String> processedResults = ConcurrentHashMap.newKeySet();
@@ -89,11 +68,15 @@ public class StringConstantQuery {
                     String file = doc.get("file");
                     String line = doc.get("line");
 
-                    if (processedResults.add(String.format("Литерал: %s, Файл: %s, Строка: %s", content, file, line))) {
+                    String logMessage = String.format("Литерал: %s, Файл: %s, Строка: %s", content, file, line);
+                    if (processedResults.add(logMessage)) {
                         documentConsumer.accept(doc);
+                        logBuilder.append(logMessage).append("\n");
                     }
                 } catch (IOException e) {
-                    logger.error("Ошибка при обработке документа: {}", e.getMessage(), e);
+                    String errorMessage = "Ошибка при обработке документа: " + e.getMessage();
+                    logger.error(errorMessage, e);
+                    logBuilder.append(errorMessage).append("\n");
                 }
             });
         }
@@ -102,8 +85,18 @@ public class StringConstantQuery {
         try {
             executor.awaitTermination(1, TimeUnit.HOURS);
         } catch (InterruptedException e) {
-            logger.error("Ошибка при ожидании завершения обработки: {}", e.getMessage(), e);
+            String errorMessage = "Ошибка при ожидании завершения обработки: " + e.getMessage();
+            logger.error(errorMessage, e);
+            logBuilder.append(errorMessage).append("\n");
         }
+    }
+
+    private static void runQuery(String queryString, String type, Consumer<Document> documentConsumer) {
+        runQueryWithConfig(queryString, type, false, documentConsumer);
+    }
+
+    private static void runFuzzyQuery(String queryString, String type, Consumer<Document> documentConsumer) {
+        runQueryWithConfig(queryString, type, true, documentConsumer);
     }
 
 
@@ -116,80 +109,53 @@ public class StringConstantQuery {
     }
 
     public static void findStringConstants(String queryString, boolean isFuzzy) {
-        findWithQuery(queryString, "StringConstant", isFuzzy, doc -> {
-            String content = doc.get("content");
-            String file = doc.get("file");
-            String line = doc.get("line");
-            logger.info("Строка: {}, Файл: {}, Строка: {}", content, file, line);
-        });
+        findWithQuery(queryString, "StringConstant", isFuzzy);
     }
 
     public static void findClass(String className, boolean isFuzzy) {
-        findWithQuery(className, "Class", isFuzzy, doc -> {
-            String content = doc.get("content");
-            String file = doc.get("file");
-            String line = doc.get("line");
-            logger.info("Класс: {}, Файл: {}, Строка: {}", content, file, line);
-        });
+        findWithQuery(className, "Class", isFuzzy);
     }
 
     public static void findMethod(String methodName, boolean isFuzzy) {
-        findWithQuery(methodName, "Method", isFuzzy, doc -> {
-            String content = doc.get("content");
-            String file = doc.get("file");
-            String line = doc.get("line");
-            logger.info("Метод: {}, Файл: {}, Строка: {}", content, file, line);
-        });
+        findWithQuery(methodName, "Method", isFuzzy);
     }
 
     public static void findInterface(String interfaceName, boolean isFuzzy) {
-        findWithQuery(interfaceName, "Interface", isFuzzy, doc -> {
-            String content = doc.get("content");
-            String file = doc.get("file");
-            String line = doc.get("line");
-            logger.info("Интерфейс: {}, Файл: {}, Строка: {}", content, file, line);
-        });
+        findWithQuery(interfaceName, "Interface", isFuzzy);
     }
 
     public static void findField(String fieldName, boolean isFuzzy) {
-        findWithQuery(fieldName, "Field", isFuzzy, doc -> {
-            String content = doc.get("content");
-            String file = doc.get("file");
-            String line = doc.get("line");
-            logger.info("Поле: {}, Файл: {}, Строка: {}", content, file, line);
-        });
+        findWithQuery(fieldName, "Field", isFuzzy);
     }
 
     public static void findLocalVariable(String variableName, boolean isFuzzy) {
-        findWithQuery(variableName, "LocalVariable", isFuzzy, doc -> {
-            String content = doc.get("content");
-            String file = doc.get("file");
-            String line = doc.get("line");
-            logger.info("Переменная: {}, Файл: {}, Строка: {}", content, file, line);
-        });
+        findWithQuery(variableName, "LocalVariable", isFuzzy);
     }
 
     public static void findLiteral(String literalValue, String type, boolean isFuzzy) {
-        findWithQuery(literalValue, type, isFuzzy, doc -> {
+        findWithQuery(literalValue, type, isFuzzy);
+    }
+
+    private static void findWithQuery(String queryString, String type, boolean isFuzzy) {
+        findWithQuery(queryString, type, isFuzzy, doc -> {
             String content = doc.get("content");
             String file = doc.get("file");
             String line = doc.get("line");
-            logger.info("Литерал: {}, Файл: {}, Строка: {}", content, file, line);
+            String logMessage = String.format("%s: %s, Файл: %s, Строка: %s", type, content, file, line);
+            logger.info(logMessage);
+            logBuilder.append(logMessage).append("\n");
         });
     }
 
     public static void main(String[] args) {
-        //indexJavaSources("src");
+        indexJavaSources("src");
 
-        float a = 3.13F;
-        boolean b = true;
-
-        findStringConstants("Hello, Lucene!", true);
+        findStringConstants("Test String", false);
         findClass("Stringdonstantuery", true);
-        findMethod("extractWithWalker", false);
-        findInterface("ExtractResultCallback", false);
-        findField("logger", false);
-        findLocalVariable("str", false);
+        findMethod("toString", false);
+        findInterface("TestInterface", false);
+        findField("testField", false);
+        findLocalVariable("content", false);
         findLiteral("100000", "IntegerLiteral", false);
         findLiteral("3.13F", "FloatLiteral", false);
         findLiteral("true", "BooleanLiteral", false);
